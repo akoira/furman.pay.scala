4228fdfef72aecfe6ad30a318beab6c282ed3546
akoyro <pervoliner@gmail.com>
2015-09-09 01:29:01 +0300

    add mongo and angular

---
+++ test/controllers/UsersIT.scala
@@ -0,0 +1,81 @@
+package controllers
+
+import java.util.concurrent.TimeUnit
+
+import org.specs2.mutable._
+import play.api.libs.json._
+import play.api.test.Helpers._
+import play.api.test._
+
+import scala.concurrent._
+import scala.concurrent.duration._
+
+
+/**
+ * You can mock out a whole application including requests, plugins etc.
+ * For more information, consult the wiki.
+ */
+class UsersIT extends Specification {
+
+  val timeout: FiniteDuration = FiniteDuration(5, TimeUnit.SECONDS)
+
+  "Users" should {
+
+    "insert a valid json" in {
+      running(FakeApplication()) {
+        val request = FakeRequest.apply(POST, "/user").withJsonBody(Json.obj(
+          "firstName" -> "Jack",
+          "lastName" -> "London",
+          "age" -> 27,
+          "active" -> true))
+        val response = route(request)
+        response.isDefined mustEqual true
+        val result = Await.result(response.get, timeout)
+        result.header.status must equalTo(CREATED)
+      }
+    }
+
+    "fail inserting a non valid json" in {
+      running(FakeApplication()) {
+        val request = FakeRequest.apply(POST, "/user").withJsonBody(Json.obj(
+          "firstName" -> 98,
+          "lastName" -> "London",
+          "age" -> 27))
+        val response = route(request)
+        response.isDefined mustEqual true
+        val result = Await.result(response.get, timeout)
+        contentAsString(response.get) mustEqual "invalid json"
+        result.header.status mustEqual BAD_REQUEST
+      }
+    }
+
+    "update a valid json" in {
+      running(FakeApplication()) {
+        val request = FakeRequest.apply(PUT, "/user/Jack/London").withJsonBody(Json.obj(
+          "firstName" -> "Jack",
+          "lastName" -> "London",
+          "age" -> 27,
+          "active" -> true))
+        val response = route(request)
+        response.isDefined mustEqual true
+        val result = Await.result(response.get, timeout)
+        result.header.status must equalTo(CREATED)
+      }
+    }
+
+    "fail updating a non valid json" in {
+      running(FakeApplication()) {
+        val request = FakeRequest.apply(PUT, "/user/Jack/London").withJsonBody(Json.obj(
+          "firstName" -> "Jack",
+          "lastName" -> "London",
+          "age" -> 27))
+        val response = route(request)
+        response.isDefined mustEqual true
+        val result = Await.result(response.get, timeout)
+        contentAsString(response.get) mustEqual "invalid json"
+        result.header.status mustEqual BAD_REQUEST
+      }
+    }
+
+  }
+}
